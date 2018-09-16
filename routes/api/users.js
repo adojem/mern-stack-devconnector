c54c01aff9ac8c04271af3ee652fd6c08be8d97e
adojem <adojemcovix@gmail.com>
2018-09-16 12:43:16 +0900

    Creating JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -52,8 +54,15 @@ router.post('/login', (req, res) => {
 
       bcrypt.compare(password, user.password).then((isMatch) => {
          if (isMatch) {
-            return res.json({ msg: 'Success' });
+            const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+            return jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) =>
+               res.json({
+                  success: true,
+                  token: `Bearer ${token}`,
+               }));
          }
+
          return res.status(400).json({ password: 'Password incorrect' });
       });
    });

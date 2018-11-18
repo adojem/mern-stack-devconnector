caa526ab5e296cbb3eb8c4f1a81b09fb54adc352
adojem <adojemcovix@gmail.com>
2018-11-18 10:52:15 +0900

    Email & Password Login and Passport JWT Authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -53,6 +56,60 @@ router.post('/register', (req, res) => {
                .catch(err => console.log(err));
          });
       });
+   });
+});
+
+/**
+ * @route   GET api/users/login
+ * @desc    Login User / Returing JWT Token
+ * @access  Public
+ */
+router.post('/login', (req, res) => {
+   const { email, password } = req.body;
+
+   // Find user by email
+   User.findOne({ email }).then((user) => {
+      // Check fo user
+      if (!user) {
+         return res.status(404).json({
+            email: 'User not found',
+         });
+      }
+
+      // Check password
+      bcrypt.compare(password, user.password).then((isMatch) => {
+         if (!isMatch) {
+            return res.status(400).json({
+               password: 'Password incorrect',
+            });
+         }
+         // User matched
+         const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+         };
+         // Sign token
+         jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+            res.json({
+               success: true,
+               token: `Bearer ${token}`,
+            });
+         });
+      });
+   });
+});
+
+/**
+ * @route   GET api/users/current
+ * @desc    Returing current user
+ * @access  Private
+ */
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+   res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
    });
 });
 

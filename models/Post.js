fca67e3358fcdcfa235c6c1f24a0f8f7a99b4313
adojem <adojemcovix@gmail.com>
2018-11-20 10:00:02 +0900

    Creating the Post Model

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema({
+   user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+   },
+   text: {
+      type: String,
+      required: true,
+   },
+   name: {
+      type: String,
+   },
+   avatar: {
+      type: String,
+   },
+   likes: [
+      {
+         user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users',
+         },
+      },
+   ],
+   comments: [
+      {
+         user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users',
+         },
+         text: {
+            type: String,
+            required: true,
+         },
+         name: {
+            type: String,
+         },
+         avatar: {
+            type: String,
+         },
+         date: {
+            tye: Date,
+            default: Date.now,
+         },
+      },
+   ],
+});
+
+const Post = mongoose.model('post', PostSchema);
+
+module.exports = Post;

a762b6b986fd2517cd390f0f333be1e567dc4aa5
adojem <adojemcovix@gmail.com>
2018-11-22 11:31:14 +0900

    Redux Store & Chrome Extension Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+//  Components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
-import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+import store from './store';
+
+// CSS
+import './App.css';
 
 class App extends Component {
    render() {
       return (
-         <Router>
-            <div className="App">
-               <Navbar />
-               <Route path="/" exact component={Landing} />
-               <div className="container">
-                  <Route path="/register" component={Register} />
-                  <Route path="/login" component={Login} />
+         <Provider store={store}>
+            <Router>
+               <div className="App">
+                  <Navbar />
+                  <Route path="/" exact component={Landing} />
+                  <div className="container">
+                     <Route path="/register" component={Register} />
+                     <Route path="/login" component={Login} />
+                  </div>
+                  <Footer />
                </div>
-               <Footer />
-            </div>
-         </Router>
+            </Router>
+         </Provider>
       );
    }
 }
